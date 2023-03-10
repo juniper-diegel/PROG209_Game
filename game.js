7b900da2e4c5e6718037610dc5609584e6e813dd
LukeD <loc.daobc@bellevuecollege.edu>
2023-03-09 16:18:43 -0800

    [LocD] margin

--- game.js
+++ game.js
@@ -34,11 +34,14 @@ const MAZE = {
 const ASSET_WIDTH = 50;
 const ASSET_HEIGHT = 50;
 
+const MARGIN_TOP = 50;
+const MARGIN_LEFT = 100;
+
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 
-canvas.width = (MAZE.maze.length + 1) * ASSET_WIDTH;
-canvas.height = (MAZE.maze[0].length + 1) * ASSET_HEIGHT;
+canvas.width = (MAZE.maze.length + 4) * ASSET_WIDTH;
+canvas.height = (MAZE.maze[0].length + 5) * ASSET_HEIGHT;
 document.body.appendChild(canvas);
 
 const backgroundImage = new Image();
@@ -50,26 +53,30 @@ treeImage.src = "assets/tree.png";
 const roadImage = new Image();
 roadImage.src = "assets/road.png";
 
-function rawMaze(maze) {
+function drawMaze(maze) {
     for (let i = 0; i < maze.length; i++) {
         for (let j = 0; j < maze[0].length; j++) {
             switch (maze[i][j]) {
                 case 1:
-                    context.drawImage(treeImage, ASSET_WIDTH * j, ASSET_HEIGHT * i, ASSET_WIDTH, ASSET_HEIGHT);
+                    context.drawImage(treeImage, ASSET_WIDTH * j + MARGIN_LEFT, ASSET_HEIGHT * i + MARGIN_TOP, ASSET_WIDTH, ASSET_HEIGHT);
                     break;
                 case 0:
-                    context.drawImage(roadImage, ASSET_WIDTH * j, ASSET_HEIGHT * i, ASSET_WIDTH, ASSET_HEIGHT);
-                    break
+                    context.drawImage(roadImage, ASSET_WIDTH * j + MARGIN_LEFT, ASSET_HEIGHT * i + MARGIN_TOP, ASSET_WIDTH, ASSET_HEIGHT);
+                    break;
             }
         }
 
     }
 }
 
+function render() {
+    context.drawImage(backgroundImage, 0, 0);
+    drawMaze(MAZE.maze);
+}
+
 
 function main() {
-    context.drawImage(backgroundImage, 0, 0)
-    rawMaze(MAZE.maze);
+    render();
     requestAnimationFrame(main);
 }
 
